a9923afbfdc087e370649e12bba7d360c0e7c5d3
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2022-11-27 21:55:21 +0200

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,48 @@
+const data = {
+    products: [
+      {
+        name: 'Kakashi',
+        slug: 'Naruto 6th Hokagi',
+        category: 'Shinopi Konoha',
+        image: '/images/p1.jpg', // 679px × 829px
+        Power: 120,
+        Kills: 1230,
+        NickName: 'El Nasikh',
+        rating: 4.5,
+      },
+      {
+        name: 'Itachi Uchiha',
+        slug: 'The Best Ninja',
+        category: 'Shinopi Konoha',
+        image: '/images/p2.jpg',
+        Power: 250,
+        Kills: 2220,
+        NickName: 'The Immortal',
+        rating: 4.7,
+      
+      },
+      {
+        name: 'Naruto',
+        slug: 'Kyobi Guy',
+        category: 'Konoha Shinubi',
+        image: '/images/p3.jpg',
+        Power: 200,
+        Kills: 30,
+        NickName: 'The Hero',
+        rating: 4.5,
+ 
+      },
+      {
+        name: 'Sasuki',
+        slug: 'The rogue ninja',
+        category: 'Shinobi konoha',
+        image: '/images/p4.jpg',
+        Power: 200,
+        Kills: 1115,
+        NickName: 'The ReninganGuy',
+        rating: 4.7,
+       
+      },
+    ],
+  };
+  export default data;
