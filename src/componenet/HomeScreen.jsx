485aeb48a5f0a3f8c63e834f85e23d369a5c9c34
1FarZ1 <91225280+1FarZ1@users.noreply.github.com>
2022-11-28 02:30:28 +0200

    adding some fun stuffs

--- src/componenet/HomeScreen.jsx
+++ src/componenet/HomeScreen.jsx
@@ -2,29 +2,145 @@ import React, { Component } from 'react';
 
 
 class HomeScreen extends Component {
+    componentDidMount() {
+        this.interval = setInterval(() => this.setState({ time: Date.now() }), 1000);
+      }
+      componentWillUnmount() {
+        clearInterval(this.interval);
+      }
+
+
     state={
-        number:(Math.random()).toFixed(2)*100
+        number:(Math.random()).toFixed(2)*100,
+        UserInput:0,
+        Counter:0,
+        time:0
     }
     render() {
         return (
             <div style={{
                 display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "100vh",
+                justifyContent:"normal",
+                alignItems:"center",
+                height: "300vh",
                 backgroundColor: "black",
                 color: "white",
                 fontSize: "50px",
                 flexDirection: "column"  
+
             }}>
                 <h1> {this.state.number}</h1>
+                {/* <input className="e-input" type="number"  placeholder="Enter Number" onSubmit={
+                    (e) => {
+                        this.setState({
+                            UserInput: e.target.value
+                        })
+                    }
+                }/> */}
+                <input
+                style={
+                    {
+                        margin: "30px",
+                        width: "300px",
+                        height: "50px",
+                        fontSize: "30px",
+                        borderRadius: "50px",
+                        outline:"none",
+                        border: "none",
+                        backgroundColor: "white",
+                        color: "black",
+                        textAlign: "center",
+
+                    }
+                }
+                
+                onSubmit={
+                    
+                        (e) => {
+                            this.setState({
+                                Counter: this.state.Counter + 1,
+                                UserInput: e.target.value
+                            })
+                        }
+                }
+      onKeyPress={(event) => {
+        if (!/[0-9]/.test(event.key)) {
+          event.preventDefault();
+          
+          
+        }
+        
+      }
+    }
+    />
+
+    <button
+    style={
+        {
+            margin: "30px",
+            width: "300px",
+            height: "50px",
+            fontSize: "30px",
+            borderRadius: "50px",
+            outline:"none",
+            border: "none",
+            backgroundColor: "white",
+            color: "black",
+            textAlign: "center",
+        }
+    }
+    type="Submit"
+    >
+        Submit
+    </button>
+    
                 <button onClick={()=>{
-                    this.setState({
+                  this.setState({
                         number:(Math.random()).toFixed(2)*100
-                    })
+                  })
                 }}>
-                press me To generate a Random Number
+                press me To Restart The Game
             </button>
+
+            <h2>
+                {
+                    this.state.UserInput > this.state.number ? "Its Higher" : "Its Lower"
+                }
+            </h2>
+
+            <h1
+                    style={
+                        {
+                          textShadow:"0 0 2px #00e92c,0 0 30px #00e92c,0px 0px 5px #00e92c, 0 0 150px #00e92c;color:#00e92c",
+backgroundcolor:"#2E8ECE",
+                        }
+                    }>
+                {
+                    this.state.UserInput === this.state.number ? "You Won" : "You Lost"
+                }
+            </h1>
+            <h2>
+                {
+                    "You Submitted " + this.state.Counter
+                    
+                }
+            </h2>
+            <h3 style={
+                {
+               
+                }
+            }>
+                {
+                    "Your Guess Was "+ this.state.UserInput
+                }
+            </h3>
+            <h1>
+                {
+                    
+                    "The time  is Running : "+ this.state.time
+                }
+                
+            </h1>
             </div>  
 
            
